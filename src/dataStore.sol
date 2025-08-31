2512a88b48f4e793ca743ff1d5fe92836e8513ae
its-sujeet <sujeetkumar7846540@gmail.com>
2025-08-31 23:17:31 +0530

    added dataStore, simpleStore,ownable

---
+++ src/dataStore.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Ownable} from "./ownable.sol";
+
+contract DataStore is Ownable{
+    mapping (address => uint256 []) addressToNum;
+
+    function store(uint256 [] calldata _num ) public {
+        require(msg.sender == owner, "You are not the contract owner");
+        addressToNum[msg.sender] = _num;
+    }
+
+    function read() public view returns (uint256 [] memory) {
+        return addressToNum[msg.sender];
+    }
+
+
+}
---
+++ src/simpleStore.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract simpleStore{
+    uint256 public favNum;
+    struct Person{
+        string name;
+        uint256 favNum;
+
+    }
+
+    function store( uint256 _favNum) public {
+        favNum= _favNum;
+    }
+
+    function retrive() private  view returns  (uint256){
+        return favNum;
+    }
+
+    // Person public  p1 = Person("harshit", 10);
+    // Person public  p2 = Person("sahil",20);
+
+    Person[] public  persons;
+
+    mapping (uint => string) public numToName;
+
+    function addPerson(string memory _name, uint _num) public {
+        persons.push(Person(_name,_num));
+        numToName[_num] = _name;
+
+    }
+
+
+
+
+
+}
